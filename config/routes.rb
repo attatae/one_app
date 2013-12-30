12d3ae1602bc6dd0c9b802480606a64a4bb8fdee
Christopher de Alba <xrq4800@gmail.com>
2013-12-30 02:54:52 -0800

    Add a hand-made sign in and up structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FounderMatch::Application.routes.draw do
+  resources :users
+  resources :sessions
+
+  root to: 'articles#index'
+  resources :articles
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +60,4 @@ FounderMatch::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
